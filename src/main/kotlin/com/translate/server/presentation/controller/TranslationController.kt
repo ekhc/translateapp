7ecc5a53db7b228b25e35168582f080f51acb0d8
eugene-cha <eugene.cha@navercorp.com>
2024-02-10 22:30:05 +0900

    Add support for handling GET requests in TranslationController.

--- src/main/kotlin/com/translate/server/presentation/controller/TranslationController.kt
+++ src/main/kotlin/com/translate/server/presentation/controller/TranslationController.kt
@@ -3,10 +3,9 @@ package com.translate.server.presentation.controller
 import com.translate.server.presentation.dto.TranslationRequestDto
 import com.translate.server.presentation.dto.TranslationResponseDto
 import com.translate.server.application.service.GoogleTranslationServiceImpl
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api")
@@ -17,5 +16,14 @@ class TranslationController(
     @PostMapping("/translate")
     fun translate(@RequestBody request: TranslationRequestDto): TranslationResponseDto? {
         return translationService.translateText(request)
+    }
+
+    @GetMapping("/index")
+    open fun index(model: Model, request: HttpServletRequest): String {
+        val requestUrl = request.requestURL.toString()
+        val webSocketAddress = requestUrl.replace("http", "ws").
+        replace("index", "customWebSocketHandler")
+        model.addAttribute("webSocket", webSocketAddress)
+        return "index"
     }
 }
