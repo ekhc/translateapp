926ddbd09ad59e20668aad5c6ad8eae77636b735
eugene-cha <eugene.cha@navercorp.com>
2024-02-16 17:49:46 +0900

    Refactor WebSocket configuration to enable message brokers and add message converters.

--- src/main/kotlin/com/translate/server/infrastructure/config/WebSocketConfig.kt
+++ src/main/kotlin/com/translate/server/infrastructure/config/WebSocketConfig.kt
@@ -1,30 +1,44 @@
 package com.translate.server.infrastructure.config
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.translate.server.infrastructure.websocket.CustomWebSocketHandler
 import com.translate.server.presentation.controller.TranslationController
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.converter.*
+import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.util.MimeTypeUtils
 import org.springframework.web.socket.WebSocketHandler
-import org.springframework.web.socket.config.annotation.EnableWebSocket
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
+import org.springframework.web.socket.config.annotation.*
 
 @Configuration
-@EnableWebSocket
-class CustomWebSocketConfig : WebSocketConfigurer {
+@EnableWebSocketMessageBroker
+class CustomWebSocketConfig : WebSocketMessageBrokerConfigurer {
     private val logger = LoggerFactory.getLogger(CustomWebSocketConfig::class.java)
 
     override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
-        logger.info("registerWebSocketHandlers is being called")
-        val handler = customWebSocketHandler()
-        val path = "/api/ws"
-        registry.addHandler(handler, path)
-        logger.info("Registered WebSocket handler: ${handler::class.java.name} at path: $path")
+        registry.addHandler(CustomWebSocketHandler(), "/chat")
+    }
+    override fun configureMessageBroker(registry: MessageBrokerRegistry) {
+        registry.enableSimpleBroker("/topic")
+        registry.setApplicationDestinationPrefixes("/app")
     }
 
-    @Bean
-    open fun customWebSocketHandler(): CustomWebSocketHandler {
-        return CustomWebSocketHandler()
+    override fun registerStompEndpoints(registry: StompEndpointRegistry) {
+        registry.addEndpoint("/chat")
+        registry.addEndpoint("/chat").withSockJS()
+    }
+
+    override fun configureMessageConverters(messageConverters: MutableList<MessageConverter>): Boolean {
+        val resolver = DefaultContentTypeResolver()
+        resolver.defaultMimeType = MimeTypeUtils.APPLICATION_JSON
+        val converter = MappingJackson2MessageConverter()
+        converter.objectMapper = ObjectMapper()
+        converter.contentTypeResolver = resolver
+        messageConverters.add(StringMessageConverter())
+        messageConverters.add(ByteArrayMessageConverter())
+        messageConverters.add(converter)
+        return false
     }
 }
